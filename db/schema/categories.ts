fea2a8d60fe4f2a3542d7466b06ce092b100a7f8
Nahuel Luca <nahuelluca20@gmail.com>
2024-01-15 15:06:53 -0300

    Update target to es6 in tsconfig.json and add generate script in package.json

---
+++ db/schema/categories.ts
@@ -0,0 +1,12 @@
+import {randomUUID} from "crypto";
+
+import {sqliteTable, text} from "drizzle-orm/sqlite-core";
+
+export const categories = sqliteTable("categories", {
+  id: text("id", {length: 36})
+    .$defaultFn(() => randomUUID())
+    .notNull()
+    .primaryKey(),
+  categoty_name: text("name", {length: 100}).notNull(),
+  description: text("description", {length: 255}).notNull(),
+});
---
+++ drizzle/0000_chemical_gladiator.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `categories` (
+	`id` text(36) PRIMARY KEY NOT NULL,
+	`name` text(100) NOT NULL,
+	`description` text(255) NOT NULL
+);
